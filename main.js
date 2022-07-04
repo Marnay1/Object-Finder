8958a6fc27a8433df5cd7cb734db12d96a6a8b19
Marnay1 <87884789+Marnay1@users.noreply.github.com>
2022-07-04 09:46:58 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,7 +1,22 @@
+status= "";
 function preload(){
 
 }
 function setup(){
     canvas= createCanvas(400, 400);
     canvas.center();
+    video= createCapture(VIDEO);
+    video.hide();
+}
+function draw(){
+    image(video, 0, 0, 400, 400);
+}
+function start(){
+    objectDetector= ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML= "Status: Detecting Objects";
+    object_name= document.getElementById("Object_Name").value;
+}
+function modelLoaded(){
+    console.log("Model Loaded!");
+    status= true;
 }
