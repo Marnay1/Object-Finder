f9ccdf22583ee281bc67b9dd42db051243153c64
Marnay1 <87884789+Marnay1@users.noreply.github.com>
2022-07-08 21:59:23 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 status= "";
+objects= [];
 function preload(){
 
 }
@@ -10,6 +11,29 @@ function setup(){
 }
 function draw(){
     image(video, 0, 0, 400, 400);
+    if(status != ""){
+        objectDetector.detect(video, gotResult);
+        for(i=0; i < objects.length; i++){
+
+            confidence= floor(objects[i].confidence * 100);
+            label= objects[i].label;
+            fill("red");
+            text(label + " " + confidence + "%", objects[i].x, objects[i].y);
+            noFill();
+            stroke("red")
+            rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height); 
+            if(object_name == label){
+                //video_holds_webcamLiveView.stop();
+                document.getElementById("detected").innerHTML= object_name + " Is Found";
+                sound= Window.speechSynthesis;
+                utterThis= new SpeechSynthesisUtterance(object_name + " Is Found");
+                sound.speak(utterThis);
+            } else{
+                status= object_name + " Not Found";
+
+            }
+        }
+    }
 }
 function start(){
     objectDetector= ml5.objectDetector('cocossd', modelLoaded);
@@ -19,4 +43,11 @@ function start(){
 function modelLoaded(){
     console.log("Model Loaded!");
     status= true;
+}
+function gotResult(error, results){
+    if(error){
+        console.log(error);
+    }
+    console.log(results);
+    objects= results;
 }
